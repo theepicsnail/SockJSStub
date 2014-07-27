d6c7968022fc14e63bd012f62a8a4a9d4b51ad0b
theepicsnail <theepicsnail@gmail.com>
2014-07-26 21:59:33 -0400

    Simplified example

--- example/server.js
+++ example/server.js
@@ -5,29 +5,32 @@ var sockjs = require('sockjs');
 var node_static = require('node-static');
 var Stub = require('SockJSStub');
 
-// 1. Echo sockjs server
+
+function handleConnection(conn){
+  // Stub the connection
+  var stub = Stub.SockJSServerStub(conn);
+  stub.on('add', function(a,b){
+    return a+b;
+  });
+  stub.on('addAsync', function(a,b) {
+    setTimeout(function(){
+      this.return(a+b);
+    }.bind(this), 1000);
+  });
+
+  stub.on('foo', function(n) {
+    stub.call('bar', [n,n*2]).then(this.return);
+  });
+}
+
+
+
 var sockjs_opts = {sockjs_url: "http://cdn.sockjs.org/sockjs-0.3.min.js"};
-
 var sockjs_echo = sockjs.createServer(sockjs_opts);
-sockjs_echo.on('connection', function(conn) {
-    var stub = Stub.SockJSServerStub(conn);
-    //stub.on('length', function(message) {
-    //  return message.length;
-    //});
-    stub.on('length', function(message) {
-      setTimeout(function(){
-        this.return("Async slow return:" + message.length);
-      }.bind(this), 1000);
-    });
-    conn.on('data', function(message) {
-      conn.write(message);
-    });
-});
+sockjs_echo.on('connection', handleConnection);
 
-// 2. Static files server
+
 var static_directory = new node_static.Server(__dirname);
-
-// 3. Usual http stuff
 var server = http.createServer();
 server.addListener('request', function(req, res) {
     static_directory.serve(req, res);
@@ -36,7 +39,7 @@ server.addListener('upgrade', function(req,res){
     res.end();
 });
 
-sockjs_echo.installHandlers(server, {prefix:'/echo'});
+sockjs_echo.installHandlers(server, {prefix:'/sjs'});
 
 console.log(' [*] Listening on 0.0.0.0:2000' );
 server.listen(2000, '0.0.0.0');
